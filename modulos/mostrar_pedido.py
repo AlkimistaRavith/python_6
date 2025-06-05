cc2ae79e921814cad725b2014b50cd4dba015971
AlkimistaRavith <ravith.d3@gmail.com>
2025-06-05 05:21:22 +0000

    casi completo

--- modulos/mostrar_pedido.py
+++ modulos/mostrar_pedido.py
@@ -2,8 +2,8 @@ def mostrar_pedido(pedido):
     ingredientes = list(pedido["ingredientes"])
     print(f"""Menu 5. REVISA TU PEDIDO
 Tu pedido es el siguiente:
-masa: {pedido['masa']}
-salsa: {pedido['salsa']}
-ingredientes:""")
+Masa: {pedido['masa']}
+Salsa: {pedido['salsa']}
+Ingredientes:""")
     for i in ingredientes:
         print(f"- {i}")
